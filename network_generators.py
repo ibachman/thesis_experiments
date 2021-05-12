38a7b02b35cdee65b17d51f38b99583418ccbf21
Ivana Bachmann <ivana.bachmann@gmail.com>
2021-05-12 10:37:59 -0400

    network models [kNN, RNG, GG] corrected. Added models [YAO, GPA, ER].

--- network_generators.py
+++ network_generators.py
@@ -6,13 +6,12 @@ import datetime
 __author__ = 'ivana'
 
 
-def generate_physical_network(n, model, coord_dict):
+def generate_physical_network(n, model, coord_dict, space=(100, 100), recursive=True):
     x = 0
     y = 1
     # establish neighbourhoods
     node_connections = []
     if model == "RNG":
-        print()
         for i in range(n):
             for j in range(i+1, n):
                 # node i data
@@ -71,7 +70,8 @@ def generate_physical_network(n, model, coord_dict):
                 if can_connect:
                     node_connections.append((node_i_name, node_j_name))
 
-    elif model == "5NN":
+    elif len(model) == 3 and "NN" in model:
+        number_of_neighbors = int(model[0])
         for i in range(n):
             # node i data
             node_i_name = "p{}".format(i)
@@ -79,6 +79,8 @@ def generate_physical_network(n, model, coord_dict):
 
             closest_to_i = []
             for j in range(n):
+                if j == i:
+                    continue
                 # node j data
                 node_j_name = "p{}".format(j)
                 loc_node_j = (coord_dict[node_j_name][x], coord_dict[node_j_name][y])
@@ -86,11 +88,14 @@ def generate_physical_network(n, model, coord_dict):
                 closest_to_i.append((get_distance(loc_node_i, loc_node_j), j))
 
             closest_to_i.sort(key=lambda tup: tup[0])
-            for w in range(1, 5):
-                j = closest_to_i[w][1]
-                k = min(i, j)
-                j = max(i, j)
-                i = k
+            for w in range(number_of_neighbors + 1):
+                k = closest_to_i[w][1]
+                if i < k:
+                    j = k
+                else:
+                    j = i
+                    i = k
+
                 if ("p{}".format(i), "p{}".format(j)) not in node_connections:
                     node_connections.append(("p{}".format(i), "p{}".format(j)))
 
@@ -107,7 +112,9 @@ def generate_physical_network(n, model, coord_dict):
                             (-60, -120): {'distance': np.infty, 'closest_node': -1},
                             (-120, -180): {'distance': np.infty, 'closest_node': -1}}
 
-            for j in range(i + 1, n):
+            for j in range(n):
+                if j == i:
+                    continue
                 # node j data
                 node_j_name = "p{}".format(j)
                 loc_node_j = (coord_dict[node_j_name][x], coord_dict[node_j_name][y])
@@ -131,19 +138,141 @@ def generate_physical_network(n, model, coord_dict):
             for area in area_buckets.keys():
                 candidate_node = area_buckets[area]['closest_node']
                 if candidate_node != -1:
-                    node_connections.append((node_i_name, candidate_node))
-    else:
-        print("ERROR")
+                    cand_node_index = int(candidate_node.replace("p", ""))
+                    if i < cand_node_index:
+                        new_edge = (node_i_name, candidate_node)
+                    else:
+                        new_edge = (candidate_node, node_i_name)
+                    if new_edge not in node_connections:
+                        node_connections.append(new_edge)
+    elif model == "GPA":
+        m = 50  # edges added in each iteration
+        alpha = 0.0002
+        t = 0
+        radx = space[x] * math.sqrt(math.log10(n)) / math.pow(n, 0.4)  # The value 0.425 can be changed.
+        rady = (space[y] / space[x]) * radx
+        areaR = math.pi * radx * rady
+        # V contains vertices at step t
+        V = {}
+        # E contains edges at step t
+        E = {}
+        # deg at step t
+        deg = {}
+        # G_0 is the empty graph (no edges, all nodes)
+        E[0] = []
+        V[0] = ["p{}".format(x) for x in list(range(n))]
+        deg[0] = {}
+        for u in V[0]:
+            deg[0][u] = 0
 
-        # set nodes ids
+        for i in range(n):
+            t += 1
+
+            deg[t] = {}
+            node_i_name = "p{}".format(i)
+            loc_node_i = (coord_dict[node_i_name][x], coord_dict[node_i_name][y])
+
+            # close_nodes = B_r(x_{t+1})
+            close_nodes = []
+            # sum_degrees = D_t(x_t)
+            sum_degrees = 0
+            # node i data
+
+            for j in range(n):
+                # node j data
+                node_j_name = "p{}".format(j)
+                loc_node_j = (coord_dict[node_j_name][x], coord_dict[node_j_name][y])
+
+                # get B_r(x_{t+1})
+                if is_within_elliptical_area(loc_node_i, loc_node_j, radx, rady):
+                    # close_nodes = B_r(x_{t+1})
+                    close_nodes.append(node_j_name)
+
+            # get V_t(x_t) = V_t intersec B_r(x_{t+1})
+            V_t_x_t_1 = [x for x in V[t - 1] if x in close_nodes]
+
+            # get D_t(x_t)
+            for node_v in V_t_x_t_1:
+                # sum_degrees = Dt(xt)
+                sum_degrees += deg[t-1][node_v]
+
+            # prob of adding edge (x_t_1, y_i)
+            prob_of_connecting_with = {}
+            for node_v in V_t_x_t_1:
+                if node_v == node_i_name:
+                    continue
+                # Pr(y_i = v)
+                prob = deg[t - 1][node_v] / max(sum_degrees, alpha * m * areaR * t)*1.0
+                prob_of_connecting_with[node_v] = prob
+            prob_of_connecting_with[node_i_name] = 1 - sum_degrees / max(sum_degrees, alpha * m * areaR * t)*1.0
+
+            # get 'm' edges
+            aux_node_list = []
+            aux_prob_list = []
+            for node_v in V_t_x_t_1:
+                aux_node_list.append(node_v)
+                aux_prob_list.append(prob_of_connecting_with[node_v])
+            y_i_list = random.choices(aux_node_list, aux_prob_list, k=m)
+            V[t] = V[t-1]
+            for u in V[t]:
+                deg[t][u] = deg[t - 1][u]
+
+            E[t] = E[t-1]
+            for y_i in y_i_list:
+                E[t].append((y_i, node_i_name))
+                deg[t][y_i] += deg[t-1][y_i] + 1
+                deg[t][node_i_name] += deg[t - 1][node_i_name] + 1
+
+        distinct_edges_dict = {}
+        for tuple in E[t]:
+            node_i = int(tuple[x].replace("p", ""))
+            node_j = int(tuple[y].replace("p", ""))
+            if node_i < node_j:
+                new_edge = tuple
+            elif node_j < node_i:
+                new_edge = (tuple[y], tuple[x])
+            else:
+                continue
+            distinct_edges_dict[new_edge] = ""
+
+        keyslist = list(distinct_edges_dict.keys())
+
+        node_connections = keyslist
+
+    elif "ER":
+        # in ER networks edges do not depend of the node allocation
+        node_connections = []
+        for i in range(n):
+            node_i_name = 'p{}'.format(i)
+            for j in range(i+1, n):
+                node_j_name = 'p{}'.format(j)
+                rand_val = random.uniform(0, 1)
+
+                if rand_val < (math.log(n) / n):
+                    node_connections.append((node_i_name, node_j_name))
+
+    # set nodes ids
     id_list = []
+    cord = []
     for i in range(n):
-        id_list.append("p{}".format(i))
+        node_i_name = "p{}".format(i)
+        id_list.append(node_i_name)
+        loc = (coord_dict[node_i_name][x], coord_dict[node_i_name][y])
+        cord.append(loc)
+
     # generate igraph graph
     graph = igraph.Graph(n)
     graph.vs['name'] = id_list
-    graph.add_edges(node_connections)
+    graph.vs['loc'] = cord
 
+    graph.add_edges(node_connections)
+    if model == "GPA" or model == "ER" and recursive:
+        number_of_connected_components = len(graph.clusters())
+        if number_of_connected_components > 1:
+            while number_of_connected_components > 1:
+                graph = generate_physical_network(n, model, coord_dict, space=space, recursive=False)
+                number_of_connected_components = len(graph.clusters())
+                print(number_of_connected_components)
     return graph
 
 
@@ -306,65 +435,126 @@ def save_edges_to_csv(edge_list, x_coordinates, y_coordinates, pg_exponent, n_de
             writer.writerow([j, k])
 
 
-def set_logic_suppliers(logic_network_nodes_ids, n, n_inter, interdep_graph):
-    # this method will choose logic suppliers within those nodes that are as interconnected as possible
-    supplier_list = {}
-    candidates_list = []
-    for k in range(len(logic_network_nodes_ids)):
-        k_neighbors = interdep_graph.neighborhood_size(vertices=['l'+str(k)])
-        if k_neighbors == n_inter:
-            candidates_list.append(k)
-    max_sample = len(candidates_list)
+def set_logic_suppliers(logic_network_nodes_ids, n_logic_suppliers):
 
-    sample = random.sample(candidates_list, min(max_sample,n))
-
-    for k in sample:
-        supplier_list[logic_network_nodes_ids[k]] = logic_network_nodes_ids[k]
-
-    if n > max_sample:
-        for i in range(n-max_sample):
-            while len(supplier_list.values()) < (i+1) :
-                k = random.randint(0,len(logic_network_nodes_ids)-1)
-                supplier_list[logic_network_nodes_ids[k]] = logic_network_nodes_ids[k]
-
-    return supplier_list.values()
+    return random.sample(logic_network_nodes_ids, n_logic_suppliers)
 
 
-def generate_logic_network(n, exponent=2.7):
+def generate_logic_network(n, exponent=2.5):
     graph = generate_power_law_graph(n, exponent, 1.0)
     id_list = []
     for i in range(n):
-        id_list.append('l'+str(i))
+        id_list.append('l'.format(i))
     graph.vs['name'] = id_list
     return graph
 
 
-def set_physical_suppliers(interdepency_network, logic_suppliers):
-    interdepency_network_ids = interdepency_network.vs['name']
+def set_physical_suppliers(interlink_network, logic_suppliers):
+    interlink_network_ids = interlink_network.vs['name']
     supplier_list = []
     for name in logic_suppliers:
-        nodes_name_neighbors = interdepency_network.neighbors(name)
+        nodes_name_neighbors = interlink_network.neighbors(name)
         for i in nodes_name_neighbors:
-            supplier_list.append(interdepency_network_ids[i])
+            supplier_list.append(interlink_network_ids[i])
     return supplier_list
 
 
-def set_interdependencies(physical_network_nodes_ids, logic_network_nodes_ids, max_number_of_interdependencies):
+def set_interdependencies(physical_network_nodes_ids, logic_network_nodes_ids, max_number_of_interdependencies,
+                          as_suppliers, mode="semi random"):
     connections = []
     physical_nodes_included = {}
+    all_supplier_counterparts = {}
+    non_supplier_logic_nodes = [x for x in logic_network_nodes_ids if x not in as_suppliers]
+    inter_degree_list = []
+    if mode == "semi random":
+        # get a list of "interdegrees" try to give the highest degrees to the supliers and then random
+        for p in range(len(logic_network_nodes_ids)):
+            inter_degree_list.append(random.randint(1, max_number_of_interdependencies))
+        print("len interdegree list: {}".format(len(inter_degree_list)))
+        # count amount of nodes with max(interdegree)
+        number_of_logic_nodes_with_max_interd = inter_degree_list.count(max_number_of_interdependencies)
+
+        print("number_of_logic_nodes_with_max_interd = {}".format(number_of_logic_nodes_with_max_interd))
+        # make suppliers have the maximum amount of interlinks possible
+        k = -1
+        current_max_inter_degree = max_number_of_interdependencies
+
+        while k != (len(as_suppliers) - 1):
+
+            for k in range(k+1, min(number_of_logic_nodes_with_max_interd, len(as_suppliers))):
+
+                # attach logic nodes to counterparts
+                logic_node = as_suppliers[k]
+                # assign max interdegree
+                amount_of_neighbours = current_max_inter_degree
+                # remove max deg occurence
+                inter_degree_list.remove(current_max_inter_degree)
+                # get physical counterparts
+                physical_neighbors = random.sample(physical_network_nodes_ids, amount_of_neighbours)
+                for physical_node in physical_neighbors:
+                    # set the connections in the connection list by id
+                    connections.append((logic_node, physical_node))
+                    # only include non-isolated nodes from the physical network
+                    physical_nodes_included[physical_node] = physical_node
+                    all_supplier_counterparts[physical_node] = ""
+
+            if current_max_inter_degree > 1:
+                current_max_inter_degree -= 1
+                number_of_logic_nodes_with_max_interd = inter_degree_list.count(current_max_inter_degree)
+
+        print("len phys suppliers: {}".format(len(list(all_supplier_counterparts.keys()))))
+        # add rest of interlinks
+
+    if mode == "provider priority":
+        used_physical_nodes = []
+        physical_nodes_left = [x for x in physical_network_nodes_ids if x not in used_physical_nodes]
+        for logic_supplier in as_suppliers:
+
+            physical_neighbors = random.sample(physical_nodes_left, max_number_of_interdependencies)
+            used_physical_nodes += physical_neighbors
+            for physical_node in physical_neighbors:
+                # set the connections in the connection list by id
+                connections.append((logic_supplier, physical_node))
+                # only include non-isolated nodes from the physical network
+                physical_nodes_included[physical_node] = physical_node
+                all_supplier_counterparts[physical_node] = ""
+            physical_nodes_left = [x for x in physical_network_nodes_ids if x not in used_physical_nodes]
+
+        print("len phys suppliers: {}".format(len(list(all_supplier_counterparts.keys()))))
+        print("physical nodes left: {}".format(len(physical_nodes_left)))
+
+        for p in range(len(physical_nodes_left)):
+            inter_degree_list.append(random.randint(1, max_number_of_interdependencies))
+
+    if mode == "full random":
+        non_supplier_logic_nodes = logic_network_nodes_ids
+        for p in range(len(logic_network_nodes_ids)):
+            inter_degree_list.append(random.randint(1, max_number_of_interdependencies))
+
+    print("len(non_supplier_logic_nodes) = {}".format(len(non_supplier_logic_nodes)))
     # for each logic node select an x between 1 and max_number_of_interdependencies
-    for logic_node in logic_network_nodes_ids:
-        amount_of_neighbours = random.randint(1, max_number_of_interdependencies)
+    for index in range(len(non_supplier_logic_nodes)):
+        amount_of_neighbours = inter_degree_list[index]
+        logic_node = non_supplier_logic_nodes[index]
+
         # select x nodes from the physical network at random
         for i in range(amount_of_neighbours):
-            physical_node_index = random.randint(0, len(physical_network_nodes_ids)-1)
+            physical_node_index = random.randint(0, len(physical_network_nodes_ids) - 1)
             physical_node = physical_network_nodes_ids[physical_node_index]
+
             # set the connections in the connection list by id
-            connections.append((logic_node,physical_node))
+            connections.append((logic_node, physical_node))
+
             # only include non-isolated nodes from the physical network
             physical_nodes_included[physical_node] = physical_node
+
+            if logic_node in as_suppliers:
+                all_supplier_counterparts[physical_node] = ""
+    if mode == "full random":
+        print("len phys suppliers: {}".format(len(list(all_supplier_counterparts.keys()))))
+
     # create the graph
-    graph = igraph.Graph(len(physical_network_nodes_ids)+len(physical_nodes_included))
+    graph = igraph.Graph(len(logic_network_nodes_ids) + len(physical_nodes_included.values()))
     graph.vs['name'] = list(physical_nodes_included.values()) + logic_network_nodes_ids
     graph.add_edges(connections)
 
@@ -457,3 +647,12 @@ def get_angle(point_1, point_2):
     oc = (y2-y1)
     ac = (x2-x1)
     return math.atan2(oc, ac)*180/math.pi
+
+
+def is_within_elliptical_area(point_1, point_2, radius_x, radius_y):
+    x = 0
+    y = 1
+    dif_x = point_1[x] - point_2[x]
+    dif_y = point_1[y] - point_2[y]
+    return ((dif_x * dif_x) / (radius_x * radius_x) + (dif_y * dif_y) / (radius_y * radius_y)) <= 1
+
