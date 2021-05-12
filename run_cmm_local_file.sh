7756ddbfd7ae8298d2a5d741277a015642029d24
Ivana Bachmann <ivana.bachmann@gmail.com>
2021-05-12 12:12:11 -0400

    .sh to run jobs from local files

---
+++ run_cmm_local_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J niclabs
+#SBATCH -c 20
+#SBATCH -p slims
+#SBATCH --output=exp_server_%A.out
+#SBATCH --mail-user=ivana@niclabs.cl (--mail-user=ivana@niclabs.cl)
+#SBATCH --mail-type=ALL
+
+ml Python/3.8.2
+
+mkdir -p test_results/degree
+mkdir -p test_results/distance
+mkdir -p test_results/random
+mkdir -p test_results/simple_graphs
+mkdir -p test_results/seismic
+
+pip3 install -r requirements.txt
+
+srun python3 -u job_manager.py -f run_legacy_LA.txt -w 20
