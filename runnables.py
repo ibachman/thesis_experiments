b43ca9286068fc6836022604c666e07fd5fa24c7
Ivana Bachmann <ivana.bachmann@gmail.com>
2021-08-04 17:52:59 -0400

    Runnable for cap0.5

--- runnables.py
+++ runnables.py
@@ -15,7 +15,7 @@ def run_test(x_coordinate, y_coordinate, exp, n_inter, n_logic_suppliers,
     #########################
     title_mod = ""
     #########################
-    capped_random = "cap0.75"
+    capped_random = "cap0.5"
     #########################
 
     attack_logic = 'logic' in attack_types
